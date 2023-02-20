42e36a95270b51186d61f65184cce695b9425399
Yoann Rodière <yoann@hibernate.org>
2023-02-20 17:55:44 +0100

    Closer to ORM tests

--- src/test/java/com/example/hibernate6/force/initializing/collection/integration/ActivationRepositoryIntegrationTest.java
+++ src/test/java/com/example/hibernate6/force/initializing/collection/integration/ActivationRepositoryIntegrationTest.java
@@ -1,8 +1,10 @@
 package com.example.hibernate6.force.initializing.collection.integration;
 
 import com.example.hibernate6.force.initializing.collection.models.*;
-import jakarta.persistence.EntityManager;
+
 import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -11,9 +13,12 @@ import org.springframework.transaction.support.TransactionTemplate;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
-import java.util.function.Supplier;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.hibernate.Session;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -26,7 +31,23 @@ class ActivationRepositoryIntegrationTest {
 
     @Test
     void in_one_transaction() {
-        inTransaction( this::createAndRetrieve );
+        inTransaction( session -> {
+			var le = new LegalEntity();
+			session.persist(le);
+			var bankAccount = new BankAccount(null, le);
+			session.persist(bankAccount);
+			var ownership = new Ownership(null, le);
+			session.persist(ownership);
+			Set<BankAccount> bankAccounts = new HashSet<>();
+			bankAccounts.add(bankAccount);
+			le.setBankAccounts(bankAccounts);
+			le.setOwnership(ownership);
+			session.merge(le);
+			var activation = new Activation(null, le, bankAccount);
+			session.persist(activation);
+			Optional<Activation> activation0 = Optional.ofNullable(session.find(Activation.class, activation.getId()));
+			assertTrue(activation0.isPresent());
+		} );
     }
 
     @Test
@@ -34,36 +55,57 @@ class ActivationRepositoryIntegrationTest {
         // it is enough to set BankAccount#legalEntity to lazy and this will pass
         // but this seems to be regression in cycle detection
     void using_separate_transactions() {
-        createAndRetrieve();
+		var le = new LegalEntity();
+		inTransaction(session -> session.persist(le));
+		var bankAccount = new BankAccount(null, le);
+		inTransaction(session -> session.persist(bankAccount));
+		var ownership = new Ownership(null, le);
+		inTransaction(session -> session.persist(ownership));
+		Set<BankAccount> bankAccounts = new HashSet<>();
+		bankAccounts.add(bankAccount);
+		le.setBankAccounts(bankAccounts);
+		le.setOwnership(ownership);
+		inTransaction(session -> session.merge(le));
+		var activation = new Activation(null, le, bankAccount);
+		inTransaction(session -> session.persist(activation));
+		Optional<Activation> activation0 = Optional.ofNullable(inTransactionReturning(session -> session.find(Activation.class, activation.getId())));
+		assertTrue(activation0.isPresent());
     }
 
     void createAndRetrieve() {
         var le = new LegalEntity();
-        inTransaction(() -> em.persist(le));
+        inTransaction(session -> session.persist(le));
         var bankAccount = new BankAccount(null, le);
-        inTransaction(() -> em.persist(bankAccount));
+        inTransaction(session -> session.persist(bankAccount));
         var ownership = new Ownership(null, le);
-        inTransaction(() -> em.persist(ownership));
+        inTransaction(session -> session.persist(ownership));
         Set<BankAccount> bankAccounts = new HashSet<>();
         bankAccounts.add(bankAccount);
         le.setBankAccounts(bankAccounts);
         le.setOwnership(ownership);
-        inTransaction(() -> em.merge(le));
+        inTransaction(session -> session.merge(le));
         var activation = new Activation(null, le, bankAccount);
-        inTransaction(() -> em.persist(activation));
-        Optional<Activation> activation0 = Optional.ofNullable(inTransaction(() -> em.find(Activation.class, activation.getId())));
+        inTransaction(session -> session.persist(activation));
+        Optional<Activation> activation0 = Optional.ofNullable(inTransactionReturning(session -> session.find(Activation.class, activation.getId())));
         assertTrue(activation0.isPresent());
     }
 
 
-    public <T> T inTransaction(Supplier<T> function) {
+    public <T> T inTransactionReturning(Function<Session, T> action) {
         transactionTemplate.setPropagationBehavior( TransactionTemplate.PROPAGATION_REQUIRED );
-        return  transactionTemplate.execute(status -> function.get());
+        return  transactionTemplate.execute(status -> action.apply(em.unwrap(Session.class)));
     }
 
-    public void inTransaction(Runnable runnable) {
-        inTransaction(() -> {
-            runnable.run();
+    public void inTransaction(Consumer<Session> action) {
+        inTransactionReturning(session -> {
+            action.accept(session);
+            return null;
+        });
+    }
+
+    public void inTransaction(Runnable action) {
+        inTransactionReturning(session -> {
+            action.run();
             return null;
         });
     }
