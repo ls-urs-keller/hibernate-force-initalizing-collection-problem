a0fa74178dd48ad5f0dcb7c5d35cdfbff80c4d20
Yoann Rodière <yoann@hibernate.org>
2023-02-20 17:58:35 +0100

    Highlight there's no need for multiple transactions

--- src/test/java/com/example/hibernate6/force/initializing/collection/integration/ActivationRepositoryIntegrationTest.java
+++ src/test/java/com/example/hibernate6/force/initializing/collection/integration/ActivationRepositoryIntegrationTest.java
@@ -11,12 +11,10 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
 import java.util.function.Consumer;
-import java.util.function.Function;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.hibernate.Session;
 
@@ -42,72 +40,19 @@ class ActivationRepositoryIntegrationTest {
 			bankAccounts.add(bankAccount);
 			le.setBankAccounts(bankAccounts);
 			le.setOwnership(ownership);
-			session.merge(le);
 			var activation = new Activation(null, le, bankAccount);
 			session.persist(activation);
-			Optional<Activation> activation0 = Optional.ofNullable(session.find(Activation.class, activation.getId()));
-			assertTrue(activation0.isPresent());
+			session.flush();
+			session.clear();
+			assertNotNull(session.find(Activation.class, activation.getId()));
 		} );
     }
 
-    @Test
-
-        // it is enough to set BankAccount#legalEntity to lazy and this will pass
-        // but this seems to be regression in cycle detection
-    void using_separate_transactions() {
-		var le = new LegalEntity();
-		inTransaction(session -> session.persist(le));
-		var bankAccount = new BankAccount(null, le);
-		inTransaction(session -> session.persist(bankAccount));
-		var ownership = new Ownership(null, le);
-		inTransaction(session -> session.persist(ownership));
-		Set<BankAccount> bankAccounts = new HashSet<>();
-		bankAccounts.add(bankAccount);
-		le.setBankAccounts(bankAccounts);
-		le.setOwnership(ownership);
-		inTransaction(session -> session.merge(le));
-		var activation = new Activation(null, le, bankAccount);
-		inTransaction(session -> session.persist(activation));
-		Optional<Activation> activation0 = Optional.ofNullable(inTransactionReturning(session -> session.find(Activation.class, activation.getId())));
-		assertTrue(activation0.isPresent());
-    }
-
-    void createAndRetrieve() {
-        var le = new LegalEntity();
-        inTransaction(session -> session.persist(le));
-        var bankAccount = new BankAccount(null, le);
-        inTransaction(session -> session.persist(bankAccount));
-        var ownership = new Ownership(null, le);
-        inTransaction(session -> session.persist(ownership));
-        Set<BankAccount> bankAccounts = new HashSet<>();
-        bankAccounts.add(bankAccount);
-        le.setBankAccounts(bankAccounts);
-        le.setOwnership(ownership);
-        inTransaction(session -> session.merge(le));
-        var activation = new Activation(null, le, bankAccount);
-        inTransaction(session -> session.persist(activation));
-        Optional<Activation> activation0 = Optional.ofNullable(inTransactionReturning(session -> session.find(Activation.class, activation.getId())));
-        assertTrue(activation0.isPresent());
-    }
-
-
-    public <T> T inTransactionReturning(Function<Session, T> action) {
-        transactionTemplate.setPropagationBehavior( TransactionTemplate.PROPAGATION_REQUIRED );
-        return  transactionTemplate.execute(status -> action.apply(em.unwrap(Session.class)));
-    }
-
     public void inTransaction(Consumer<Session> action) {
-        inTransactionReturning(session -> {
-            action.accept(session);
-            return null;
-        });
-    }
-
-    public void inTransaction(Runnable action) {
-        inTransactionReturning(session -> {
-            action.run();
-            return null;
-        });
+		transactionTemplate.execute( status -> {
+			action.accept( em.unwrap( Session.class ) );
+			return null;
+		} );
     }
 
 }
